d0482cebb8626e3457af7e450e0bcdd0da3c2f79
TobiasF92 <TobiasFricke1@gmx.de>
2026-03-31 21:58:06 +0200

    added basic FS25_VariableTirePressure function support

--- src/misc/InteractiveFunctions_externalMods.lua
+++ src/misc/InteractiveFunctions_externalMods.lua
@@ -31,3 +31,100 @@ local function getExternalModClass(modEnvironmentName, modClassName)
 
     return modEnvironment[modClassName], false
 end
+
+----------------------------------------------- FS25_VariableTirePressure ----------------------------------------------
+
+---Returns true if given axle type is in road, false otherwise
+---@param target Vehicle Instance of vehicle
+---@param axle string Axle type
+---@return boolean isRoadMode
+local function isAxleInRoadMode(target, axle)
+    local spec = target.spec_variableTirePressure
+    if spec == nil or axle == nil then
+        return false
+    end
+
+    if spec._vtpAxleState == nil then
+        return spec.isRoadMode
+    end
+
+    local axleState = spec._vtpAxleState[axle]
+    return axleState.isRoadMode == nil or axleState.isRoadMode
+end
+
+---VTP_TIRE_PRESSURE_TOGGLE_REAR
+InteractiveFunctions.addFunction("VTP_TIRE_PRESSURE_TOGGLE_REAR", {
+    posFunc = function(target, data, noEventSend)
+        local VariableTirePressure = getExternalModClass("FS25_VariableTirePressure", "VariableTirePressure")
+
+        if noEventSend or VariableTirePressure == nil then
+            return
+        end
+
+        if target.vtpToggleTractorAxleMode ~= nil then
+            target:vtpToggleTractorAxleMode("rear")
+        end
+    end,
+    updateFunc = function(target, data)
+        local VariableTirePressure = getExternalModClass("FS25_VariableTirePressure", "VariableTirePressure")
+
+        if VariableTirePressure ~= nil then
+            return isAxleInRoadMode(target, "rear")
+        end
+        return nil
+    end
+})
+---VTP_TIRE_PRESSURE_TOGGLE_FRONT
+InteractiveFunctions.addFunction("VTP_TIRE_PRESSURE_TOGGLE_FRONT", {
+    posFunc = function(target, data, noEventSend)
+        local VariableTirePressure = getExternalModClass("FS25_VariableTirePressure", "VariableTirePressure")
+
+        if noEventSend or VariableTirePressure == nil then
+            return
+        end
+
+        if target.vtpToggleTractorAxleMode ~= nil then
+            target:vtpToggleTractorAxleMode("front")
+        end
+    end,
+    updateFunc = function(target, data)
+        local VariableTirePressure = getExternalModClass("FS25_VariableTirePressure", "VariableTirePressure")
+
+        if VariableTirePressure ~= nil then
+            return isAxleInRoadMode(target, "front")
+        end
+        return nil
+    end
+})
+---VTP_TIRE_PRESSURE_TOGGLE_ALL
+InteractiveFunctions.addFunction("VTP_TIRE_PRESSURE_TOGGLE_ALL", {
+    posFunc = function(target, data, noEventSend)
+        local VariableTirePressure = getExternalModClass("FS25_VariableTirePressure", "VariableTirePressure")
+
+        if noEventSend or VariableTirePressure == nil then
+            return
+        end
+
+        if target.vtpToggleTractorAxleMode ~= nil then
+            local frontAxleInRoadMode = isAxleInRoadMode(target, "front")
+            local rearAxleInRoadMode = isAxleInRoadMode(target, "rear")
+
+            if frontAxleInRoadMode == rearAxleInRoadMode then
+                target:vtpToggleTractorAxleMode("front")
+                target:vtpToggleTractorAxleMode("rear")
+            elseif frontAxleInRoadMode then
+                target:vtpToggleTractorAxleMode("front")
+            elseif rearAxleInRoadMode then
+                target:vtpToggleTractorAxleMode("rear")
+            end
+        end
+    end,
+    updateFunc = function(target, data)
+        local VariableTirePressure = getExternalModClass("FS25_VariableTirePressure", "VariableTirePressure")
+
+        if VariableTirePressure ~= nil then
+            return isAxleInRoadMode(target, "front") and isAxleInRoadMode(target, "rear")
+        end
+        return nil
+    end
+})
